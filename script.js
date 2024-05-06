2488204172abcd620167622ea9a3d58cd5a1c600
Yashh~ <yash.aanand06@gmail.com>
2024-05-06 16:46:32 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,124 @@
+const gameState = true;
+
+const cr = document.querySelector('.rock');
+const cp = document.querySelector('.paper');
+const cs = document.querySelector('.scissor');
+
+const ym = document.querySelector('#ym');
+const cm = document.querySelector('#cm');
+const wm = document.querySelector('.final');
+
+const pscore = document.querySelector('#playerscore')
+const cscore = document.querySelector('#computerscore')
+
+const submit = document.querySelector('.submit')
+const reset = document.querySelector('.reset')
+
+let psc = 0;
+let csc = 0;
+
+
+cr.addEventListener('click', () => {
+    pm ='Rock';
+    clear()
+    cr.classList.add('chosen')
+
+})
+cp.addEventListener('click', () => {
+    pm ='Paper';
+    clear()
+    cp.classList.add('chosen')
+
+})
+cs.addEventListener('click', () => {
+    pm ='Scissors';
+    clear()
+    cs.classList.add('chosen')
+
+})
+
+submit.addEventListener('click', () => {
+    playGame(pm)
+})
+
+reset.addEventListener('click', () => {
+    psc = 0;
+    csc = 0;
+    pm = ''
+    cscore.innerHTML = `${csc}`
+    pscore.innerHTML = `${psc}`
+    ym.innerHTML = 'TBD'
+    cm.innerHTML = 'TBD'
+    wm.innerHTML = 'Pick you Choice!'
+    clear()
+
+})
+
+const clear = function(){
+    cr.classList.remove('chosen')
+    cs.classList.remove('chosen')
+    cp.classList.remove('chosen')
+}
+
+const playGame = function(yom) {
+    const choices = ['Rock','Paper','Scissors'];
+    ym.innerText = `${yom}`;
+    let com = choices[Math.floor(Math.random()*3)];
+    cm.innerHTML = `${com}`;
+    console.log(yom)
+    console.log(com)
+
+    if (yom == 'Rock'){
+            if (com == 'Rock'){
+                wm.innerHTML = 'Tie!'
+
+            } else if(com == 'Paper'){
+                wm.innerHTML = 'Computer Wins!'
+                cwin()
+
+            } else if(com == 'Scissors'){
+                wm.innerHTML = 'You Win!'
+                console.log('Player')
+                pwin()
+            }
+        } else if(yom == 'Paper'){
+            if (com == 'Rock'){
+                wm.innerHTML = 'You Win!'
+                pwin()
+
+            } else if(com == 'Paper'){
+                wm.innerHTML = 'Tie!'
+
+            } else if(com == 'Scissors'){
+                wm.innerHTML = 'Computer Wins!'
+                cwin()
+            }
+        }else if(yom == 'Scissors'){
+            if (com == 'Rock'){
+                wm.innerHTML = 'Computer Wins!'
+                cwin()
+
+            } else if(com == 'Paper'){
+                wm.innerHTML = 'You Win!'
+                pwin()
+
+            } else if(com == 'Scissors'){
+                wm.innerHTML = 'Tie!'
+            }
+        
+    }   
+}
+        
+const pwin = function (){
+    psc++;
+    pscore.innerText = `${psc}`
+}
+
+const cwin = function (){
+    csc++;
+    cscore.innerText = `${csc}`
+}
+
+
+
+
